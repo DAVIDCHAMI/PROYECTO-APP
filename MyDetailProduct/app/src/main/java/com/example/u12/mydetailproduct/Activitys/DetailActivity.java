119c3ab56f8db2c445662fa4ad42e8826ce41869
pjsilva613 <pjsilva613@gmail.com>
2019-01-17 21:07:01 -0500

    Se realiza la implementación del detalle del producto

--- MyDetailProduct/app/src/main/java/com/example/u12/mydetailproduct/Activitys/DetailActivity.java
+++ MyDetailProduct/app/src/main/java/com/example/u12/mydetailproduct/Activitys/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.u12.mydetailproduct.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.u12.mydetailproduct.R;
 import com.example.u12.mydetailproduct.models.Product;
@@ -9,6 +11,7 @@ import com.example.u12.mydetailproduct.models.Product;
 public class DetailActivity extends AppCompatActivity {
 
     private Product product;
+    private TextView  txtnombre,txtdescripcion,txtcantidad,txtprecio,txtmarca;
 
 
 
@@ -19,7 +22,23 @@ public class DetailActivity extends AppCompatActivity {
         getIntent().getSerializableExtra("product");
 
         product= (Product) getIntent().getSerializableExtra("product");
+        txtnombre = findViewById(R.id.txtNombre1);
+        txtdescripcion = findViewById(R.id.txtdescripcion1);
+        txtcantidad = findViewById(R.id.txtcantidad1);
+        txtprecio = findViewById(R.id.txtprecio1);
+        txtmarca = findViewById(R.id.txtmarca1);
 
 
+        txtnombre.setText(product.getProduct().toString());
+        txtdescripcion.setText(product.getProductDescription().toString());
+        txtcantidad.setText(Integer.toString(product.getQuantity()).toString());
+        txtprecio.setText(Integer.toString(product.getPrice()).toString());
+        txtmarca.setText(product.getBrand().toString());
+
+    }
+
+    public void onClickRegresar(View view) {
+
+       finish();
     }
 }
